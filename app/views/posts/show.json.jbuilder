51b062ad395c745a7e39ac77e7c8584ffc19ded5
Chinedu Abalogu <chineduabalogu@yahoo.com>
2016-01-25 15:19:48 +0100

    ckeditor test

---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :name, :title, :description, :content, :created_at, :updated_at
---
+++ db/migrate/20160125132438_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.string :title
+      t.text :description
+      t.text :content
+
+      t.timestamps null: false
+    end
+  end
+end
